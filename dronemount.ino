3bc32494af2e04f8771077cc6a0782573393d1fd
Sjokofant <Sjokofant@users.noreply.github.com>
2018-12-17 15:15:27 +0100

    Add files via upload

---
+++ dronemount.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+int pos;
+Servo servo;
+
+void setup() {
+  servo.attach(14);
+  Serial.begin(9600);
+  pos = 30;                             // Sets orientation of the camera  
+  servo.write(pos); 
+}
+
+void loop() {
+    while(Serial.available()){          // checks if serial is available 
+        int data = Serial.read();       // Reads from port
+
+        switch (data){
+        case 'U':
+            pos++;
+            servo.write(pos);           // tell servo to go to position in variable 'pos'
+            delay(10); 
+        break;
+    
+        case 'D':
+            pos--;
+            servo.write(pos);               
+            
+            delay(10); 
+        break;
+        }
+    }
+} 
